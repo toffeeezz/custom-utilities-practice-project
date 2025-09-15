c2aaa75115706e157f2b8958427e2651c722aa33
ttoaffers <animeone56@gmail.com>
2025-09-15 21:52:50 +0800

    Added live position edit

--- src/DebugUtils/NodeDebug.java
+++ src/DebugUtils/NodeDebug.java
@@ -1,9 +1,11 @@
 package DebugUtils;
 
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
+import static DebugUtils.SystemPrints.*;
 
 
 /**
@@ -46,6 +48,79 @@ public class NodeDebug {
                     pane.setBorder(new Border(new BorderStroke(debugColor, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(thickness), new Insets(0,0,0,0))));
                 }else{
                     pane.setBorder(null);
+                }
+            }
+        });
+    }
+
+    /**
+     * Incrementally adjusts a node with a root container based on the amount using the following binds:
+     * <pre></pre>
+     * {@code I} UP, {@code K} DOWN, {@code J} LEFT, {@code L} RIGHT
+     *
+     * @param node The node to be adjusted
+     * @param amount The amount of displacement
+     */
+    public static void translatePos(Node node, double amount){
+        node.setFocusTraversable(true);
+        node.requestFocus();
+        node.setOnMouseClicked(_-> node.requestFocus());
+        node.setOnKeyPressed(keyEvent -> {
+            switch (keyEvent.getCode()){
+                case KeyCode.I -> {
+                    node.setTranslateY(node.getTranslateY() - amount);
+                    linePrint("PosX (ORIGIN): " + node.getTranslateX() + "\nPosY (ORIGIN): " + node.getTranslateY());
+
+                }
+                case KeyCode.K -> {
+                    node.setTranslateY(node.getTranslateY() + amount);
+                    linePrint("PosX (ORIGIN): " + node.getTranslateX() + "\nPosY (ORIGIN): " + node.getTranslateY());
+
+                }
+                case KeyCode.J -> {
+                    node.setTranslateX(node.getTranslateX() - amount);
+                    linePrint("PosX (ORIGIN): " + node.getTranslateX() + "\nPosY (ORIGIN): " + node.getTranslateY());
+
+                }
+                case KeyCode.L -> {
+                    node.setTranslateX(node.getTranslateX() + amount);
+                    linePrint("PosX (ORIGIN): " + node.getTranslateX() + "\nPosY (ORIGIN): " + node.getTranslateY());
+                }
+            }
+        });
+    }
+
+    /**
+     * Incrementally adjusts a node without a root container based on the amount using the following binds:
+     * <pre></pre>
+     * {@code I} UP, {@code K} DOWN, {@code J} LEFT, {@code L} RIGHT
+     * @param node The node to be adjusted
+     * @param amount The amount of displacement
+     */
+    public static void adjustPos(Node node, double amount){
+        node.setFocusTraversable(true);
+        node.requestFocus();
+        node.setOnMouseClicked(_-> node.requestFocus());
+        node.setOnKeyPressed(keyEvent -> {
+            switch (keyEvent.getCode()){
+                case KeyCode.I -> {
+                    node.setLayoutY(node.getLayoutY() - amount);
+                    linePrint("PosX (ORIGIN): " + node.getLayoutX() + "\nPosY (ORIGIN): " + node.getLayoutY());
+
+                }
+                case KeyCode.K -> {
+                    node.setLayoutY(node.getLayoutY() + amount);
+                    linePrint("PosX (ORIGIN): " + node.getLayoutX() + "\nPosY (ORIGIN): " + node.getLayoutY());
+
+                }
+                case KeyCode.J -> {
+                    node.setLayoutX(node.getLayoutX() - amount);
+                    linePrint("PosX (ORIGIN): " + node.getLayoutX() + "\nPosY (ORIGIN): " + node.getLayoutY());
+
+                }
+                case KeyCode.L -> {
+                    node.setLayoutX(node.getLayoutX() + amount);
+                    linePrint("PosX (ORIGIN): " + node.getLayoutX() + "\nPosY (ORIGIN): " + node.getLayoutY());
                 }
             }
         });
